67bb6d846bc3dc6f4173df1a09df6ef37e2b7e2a
Gustavo Walther Paredes Quispe <131079911+gusParedes25@users.noreply.github.com>
2026-01-21 22:48:25 -0500

    Update constant.ts

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -21,19 +21,8 @@ export const FechaLugar = {
     Mes: "02",
     Anio: "2026",
     Hora: "11:30 am",
-}
-
-export const FechaLugar = {
-    Titulo_seccion: "Ceremonia Civil",
-    Lugar: "Fundo alto del Rio",
-    Distrito: "Prologación av. caracas, antes de cruzar el puente",
-    Titulo_seccion_2: "Fecha y hora del Matrimonio Civil:",
-    DiaSemana: "Sábado",
-    Dia: "21",
-    Mes: "02",
-    Anio: "2026",
-    Hora: "11:30 am",
-}
+    punto: "export const FechaLugar",
+};
 
 export const Portada = {
     Imagen: "https://images.unsplash.com/photo-1523438885200-e635ba2c371e",
